39aaa46154dee38404969a9a8a2ac970aace02e4
RubenCorreia <ruben@casa-portuguesa.de>
2016-11-18 18:21:49 +0000

    Update populate.sql
    Changed order

--- Delivery_3/populate.sql
+++ Delivery_3/populate.sql
@@ -1,40 +1,38 @@
-INSERT INTO User (nif, nome, telefone) VALUES ('11111','Joaquim','987765432');
-insert into User(nif, nome, telefone) values ('22222', 'Raquel', '999777666');
-
-INSERT INTO Fiscal(id, empresa) values ('123','sical');
-INSERT INTO Fiscal(id, empresa) values ('456','delta');
-
-insert into Edificio(morada) values ('rua da igreja');
-insert into Edificio(morada) values ('rua da quinta');
-
-insert into Arrenda(morada, codigo, nif) values ('rua da igreja','898','11111');
-insert into Arrenda(morada, codigo, nif) values ('rua da quinta','989','22222');
-
-insert into Fiscaliza(id, morada, codigo) values ('123','rua da igreja','898');
-insert into Fiscaliza(id, morada, codigo) values ('456', 'rua da quinta', '989');
-
-insert into Espaco(morada, codigo) values ('rua da igreja','898');
-insert into Espaco(morada, codigo) values ('rua da quinta', '989');
-
-insert into Posto(morada, codigo, codigo_espaco) values ('rua da igreja','898','898');
-insert into Posto(morada, codigo, codigo_espaco) values ('rua da quinta', '989','989');
-
-insert into Alugavel(morada, codigo) values ('rua da igreja', '898');
-insert into Alugavel(morada, codigo) values ('rua da quinta', '989');
-
-insert into Oferta(morada, codigo, data_inicio, data_fim, tarifa) values ('rua da igreja','898', '2017-11-18', '2017-11-19','300');
-insert into Oferta(morada, codigo, data_inicio, data_fim, tarifa) values ('rua da quinta', '989', '2017-11-19', '2017-11-20','200');
-
-insert into Aluga(morada, codigo, data_inicio, nif, numero) values ('rua da igreja', '898', '2017-11-18', '11111', '1');
-insert into Aluga(morada, codigo, data_inicio, nif, numero) values ('rua da quinta', '989', '2017-11-19', '22222', '2');
-
-insert into Paga(numero, data, metodo) values ('1','2017-11-18','CASH');
-insert into Paga(numero, data, metodo) values ('2','2017-11-19','MB');
-
 INSERT INTO Reserva (numero) VALUES (1);
 INSERT INTO Reserva (numero) VALUES (2);
 
+INSERT INTO User (nif, nome, telefone) VALUES ('11111','Joaquim','987765432');
+INSERT INTO User(nif, nome, telefone) VALUES ('22222', 'Raquel', '999777666');
+
+INSERT INTO Fiscal(id, empresa) VALUES ('123','sical');
+INSERT INTO Fiscal(id, empresa) VALUES ('456','delta');
+
+INSERT INTO Edificio(morada) VALUES ('rua da igreja');
+INSERT INTO Edificio(morada) VALUES ('rua da quinta');
+
+INSERT INTO Alugavel(morada, codigo) VALUES ('rua da igreja', '898');
+INSERT INTO Alugavel(morada, codigo) VALUES ('rua da quinta', '989');
+
+INSERT INTO Arrenda(morada, codigo, nif) VALUES ('rua da igreja','898','11111');
+INSERT INTO Arrenda(morada, codigo, nif) VALUES ('rua da quinta','989','22222');
+
+INSERT INTO Fiscaliza(id, morada, codigo) VALUES ('123','rua da igreja','898');
+INSERT INTO Fiscaliza(id, morada, codigo) VALUES ('456', 'rua da quinta', '989');
+
+INSERT INTO Espaco(morada, codigo) VALUES ('rua da igreja','898');
+INSERT INTO Espaco(morada, codigo) VALUES ('rua da quinta', '989');
+
+INSERT INTO Posto(morada, codigo, codigo_espaco) VALUES ('rua da igreja','898','898');
+INSERT INTO Posto(morada, codigo, codigo_espaco) VALUES ('rua da quinta', '989','989');
+
+INSERT INTO Oferta(morada, codigo, data_inicio, data_fim, tarifa) VALUES ('rua da igreja','898', '2017-11-18', '2017-11-19','300');
+INSERT INTO Oferta(morada, codigo, data_inicio, data_fim, tarifa) VALUES ('rua da quinta', '989', '2017-11-19', '2017-11-20','200');
+
+INSERT INTO Aluga(morada, codigo, data_inicio, nif, numero) VALUES ('rua da igreja', '898', '2017-11-18', '11111', '1');
+INSERT INTO Aluga(morada, codigo, data_inicio, nif, numero) VALUES ('rua da quinta', '989', '2017-11-19', '22222', '2');
+
+INSERT INTO Paga(numero, data, metodo) VALUES ('1','2017-11-18','CASH');
+INSERT INTO Paga(numero, data, metodo) VALUES ('2','2017-11-19','MB');
+
 INSERT INTO Estado(estado) VALUES (‘aceite’);
 INSERT INTO Estado(estado) VALUES (‘pendente’);
-
-
