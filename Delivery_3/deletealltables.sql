94d66029d339e0d3c270438fbaab5ebaff468d72
RubenCorreia <ruben@casa-portuguesa.de>
2016-11-18 17:45:53 +0000

    Create deletealltables.sql
    File to delete all tables

---
+++ Delivery_3/deletealltables.sql
@@ -0,0 +1,25 @@
+DROP TABLE Estado;
+
+DROP TABLE Paga;
+
+DROP TABLE Aluga;
+
+DROP TABLE Oferta;
+
+DROP TABLE Posto;
+
+DROP TABLE Espaco;
+
+DROP TABLE Fiscaliza;
+
+DROP TABLE Arrenda;
+
+DROP TABLE Alugavel;
+
+DROP TABLE Edificio;
+
+DROP TABLE Fiscal;
+
+DROP TABLE User;
+
+DROP TABLE Reserva;
