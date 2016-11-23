75557b53cbdcbb80ac756e19def94895bb2d6122
Joaquim Esteves <joaquimbve@hotmail.com>
2016-11-23 18:12:49 +0000

    99 little buuuuuuugs

--- Delivery_3/web/ServerSide.php
+++ Delivery_3/web/ServerSide.php
@@ -77,33 +77,6 @@ try{
 		}
 	}
 
-	//Connection to DB
-	$host="db.ist.utl.pt";
-	$user="ist177020"; // Replace if needed
-	$password="fuckyou"; //TODO: FIX THIS DON'T EVEN THINK ABOUT SENDING IT TO THE PROF LIKE THIS IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	$dbname = $user;
-	// $connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); 
-	$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo("<p>Welcome to InstantOffice</p>\n");
-	// array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-	echo("<p>Connected to MySQL database $dbname on $host as user $user</p>\n");
-	//switch utiliza a funçao get_post_action com os names dos metodos post neste caso atualizar
-	switch (get_post_action('login')) {
-		
-		//Quando o utilizador faz login, as variaveis deste são passadas para as do php
-		case'login':
-		$username = $_POST["username"];
-		$nif = $_POST["nif"];
-		$phone_number = $_POST["phone_number"];
-		testLogin($username,$nif,$phone_number);
-		//Give the session the variables
-		$_SESSION['username'] = $username; 
-		$_SESSION['nif'] = $nif;
-		$_SESSION['phone_number'] = $phone_number; 
-		break; 
-	}
-
 	function testLogin($input_username,$input_nif,$input_phone) {
 		// Verify login credentials
 		$DBusrn="";
@@ -129,6 +102,33 @@ try{
 			exit;
 		}
 		echo "<p>Valid Credentials! </p>\n";
+	}
+	
+	//Connection to DB
+	$host="db.ist.utl.pt";
+	$user="ist177020"; // Replace if needed
+	$password="fuckyou"; //TODO: FIX THIS DON'T EVEN THINK ABOUT SENDING IT TO THE PROF LIKE THIS IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	$dbname = $user;
+	// $connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); 
+	$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo("<p>Welcome to InstantOffice</p>\n");
+	// array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+	echo("<p>Connected to MySQL database $dbname on $host as user $user</p>\n");
+	//switch utiliza a funçao get_post_action com os names dos metodos post neste caso atualizar
+	switch (get_post_action('login')) {
+		
+		//Quando o utilizador faz login, as variaveis deste são passadas para as do php
+		case'login':
+		$username = $_POST["username"];
+		$nif = $_POST["nif"];
+		$phone_number = $_POST["phone_number"];
+		testLogin($username,$nif,$phone_number);
+		//Give the session the variables
+		$_SESSION['username'] = $username; 
+		$_SESSION['nif'] = $nif;
+		$_SESSION['phone_number'] = $phone_number; 
+		break; 
 	}
 }
 catch(PDOException $e) {
