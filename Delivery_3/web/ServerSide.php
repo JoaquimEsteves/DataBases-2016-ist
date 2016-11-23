3a9f801fd059e12a68cde5defb918a5e4799c940
Joaquim Esteves <joaquimbve@hotmail.com>
2016-11-23 16:59:44 +0000

    plz

--- Delivery_3/web/ServerSide.php
+++ Delivery_3/web/ServerSide.php
@@ -28,47 +28,47 @@ try{
 	echo("<p>Connected to MySQL database {$dbname} on {$host} as user {$user}</p>\n");
 
 	// switch utiliza a funçao get_post_action com os names dos metodos post neste caso atualizar
-	switch (get_post_action('login')) {
+	// switch (get_post_action('login')) {
 		
 		//Quando o utilizador faz login, as variaveis deste são passadas para as do php
-		case'login':
-		$username = $_POST["username"];
-		$nif = $_POST["nif"];
-		$phone_number = $_POST["phone_number"];
-		testLogin($username,$nif,$phone_number);
+		// case'login':
+		// $username = $_POST["username"];
+		// $nif = $_POST["nif"];
+		// $phone_number = $_POST["phone_number"];
+		// testLogin($username,$nif,$phone_number);
 		// Give the session the variables
-		$_SESSION['username'] = $username; 
-		$_SESSION['nif'] = $nif;
-		$_SESSION['phone_number'] = $phone_number; 
-		break; 
-	}
+		// $_SESSION['username'] = $username; 
+		// $_SESSION['nif'] = $nif;
+		// $_SESSION['phone_number'] = $phone_number; 
+		// break; 
+	// }
 
-	function testLogin($input_username,$input_nif,$input_phone) {
-		//Verify login credentials
-		$DBusrn="";
-		$DBphone = -1;
-		echo"<p>Validating credentials of " . $input_username"</p>\n";
-		//Get name and phone number of the given user
-		$sql = "SELECT * FROM User WHERE nif=" . $input_nif; 
-		$result = $connection->query($sql);
-		if (!$result) {
-			echo("<p> ERROR:[Could not execute this query]:($sql)<p>");
-			exit();
-		}
-		foreach($result as $i){
-			$DBusrn = $i["nome"];
-			$DBphone = $i["telefone"];
-		}
-		if ($DBusrn != $input_username OR empty($DBusrn) OR $DBphone != $input_phone OR $DBphone == -1) {
-			echo "<p>Invalid credentials! Exit!</p>\n";
-			echo("<form action='login.html'>
-			<input type='submit' value='Try again' />
-			</form>");
-			$connection = null;
-			exit;
-		}
-		echo "<p>Valid Credentials! </p>\n";
-	}
+	// function testLogin($input_username,$input_nif,$input_phone) {
+		// Verify login credentials
+		// $DBusrn="";
+		// $DBphone = -1;
+		// echo"<p>Validating credentials of " . $input_username"</p>\n";
+		// Get name and phone number of the given user
+		// $sql = "SELECT * FROM User WHERE nif=" . $input_nif; 
+		// $result = $connection->query($sql);
+		// if (!$result) {
+			// echo("<p> ERROR:[Could not execute this query]:($sql)<p>");
+			// exit();
+		// }
+		// foreach($result as $i){
+			// $DBusrn = $i["nome"];
+			// $DBphone = $i["telefone"];
+		// }
+		// if ($DBusrn != $input_username OR empty($DBusrn) OR $DBphone != $input_phone OR $DBphone == -1) {
+			// echo "<p>Invalid credentials! Exit!</p>\n";
+			// echo("<form action='login.html'>
+			// <input type='submit' value='Try again' />
+			// </form>");
+			// $connection = null;
+			// exit;
+		// }
+		// echo "<p>Valid Credentials! </p>\n";
+	// }
 	catch(PDOException $e) {
 		echo("<p>ERROR: {$e->getMessage() } </p>")
 	}
