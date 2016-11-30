1cfbb3004b68e51e0c4ecb3d82e271b6ae68dbfd
RubenCorreia <ruben@casa-portuguesa.de>
2016-11-30 13:16:11 +0000

    Update queries.sql

--- Delivery_3/queries.sql
+++ Delivery_3/queries.sql
@@ -1,25 +1,25 @@
 //a)
 SELECT DISTINCT e.morada, e.codigo 
-FROM Espaco e NATURAL JOIN Posto 
-WHERE  e.morada NOT IN (SELECT a.morada FROM Aluga
+FROM espaco e NATURAL JOIN posto 
+WHERE  e.morada NOT IN (SELECT a.morada FROM aluga a);
 
 //b)
 SELECT  a.morada, COUNT(a.morada) AS Amount_of_reservations
-FROM  Aluga a NATURAL JOIN Edificio
+FROM  aluga a NATURAL JOIN edificio
 GROUP BY morada 
-HAVING Amount_of_reservations >= (SELECT COUNT(x.numero)/2 FROM Reserva x);
+HAVING Amount_of_reservations >= (SELECT COUNT(x.numero)/2 FROM reserva x);
 
 //c)
 SELECT u.nome
-FROM User u NATURAL JOIN Arrenda a
+FROM user u NATURAL JOIN arrenda a
 WHERE EXISTS
-(SELECT COUNT(f1.id) FROM Arrenda NATURAL JOIN Fiscaliza f1 
+(SELECT COUNT(f1.id) FROM arrenda NATURAL JOIN fiscaliza f1 
 GROUP BY f1.id 
 HAVING f1.id IN
-(SELECT COUNT(f2.id) FROM Fiscaliza f2 GROUP BY f2.id));
+(SELECT COUNT(f2.id) FROM fiscaliza f2 GROUP BY f2.id));
 
 //d)
 SELECT DISTINCT e.morada, SUM(o.tarifa)*365 AS maxpayed 
-FROM Paga p NATURAL JOIN Oferta o NATURAL JOIN Espaco e NATURAL JOIN Aluga 
+FROM paga p NATURAL JOIN oferta o NATURAL JOIN espaco e NATURAL JOIN aluga 
 WHERE p.data LIKE '%2016%'
 GROUP BY o.tarifa;
