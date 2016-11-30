670eca4499e6de06f98c2e1775a907c87886a1ad
Joaquim Esteves <joaquimbve@hotmail.com>
2016-11-30 18:50:13 +0000

    stupid sessions!

--- Delivery_3/web/ServerSide.php
+++ Delivery_3/web/ServerSide.php
@@ -84,6 +84,24 @@ $old_error_handler = set_error_handler("ErrorHandler");
 try{
 
 	session_start();
+	if (!isset($_SESSION['EXPIRES']) || $_SESSION['EXPIRES'] < time()+3600) {
+		// Unset all of the session variables.
+		$_SESSION = array();
+
+		// If it's desired to kill the session, also delete the session cookie.
+		// Note: This will destroy the session, and not just the session data!
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params["path"], $params["domain"],
+				$params["secure"], $params["httponly"]
+			);
+		}
+
+		// Finally, destroy the session.
+		session_destroy();
+	}
+	$_SESSION['EXPIRES'] = time() + 3600;
 	
 	//Connection to DB
 	$host="db.ist.utl.pt";
