9cb7a839bb48b099acec45e2b809220d646b5c24
raquelpinto <raquelpintopereira6@gmail.com>
2016-12-01 16:29:02 +0000

    Update

--- Delivery_3/triggers_storedproc.sql
+++ Delivery_3/triggers_storedproc.sql
@@ -1,24 +1,22 @@
+DROP TRIGGER IF EXISTS ins_dataOferta;
+DROP TRIGGER IF EXISTS ins_dataPagamento;
 --a)
 DELIMITER //
 CREATE TRIGGER ins_dataOferta BEFORE INSERT ON oferta
 	FOR EACH ROW
 	BEGIN
 		DECLARE error_message varchar(255);
-
-		IF NEW.data_inicio NOT IN (SELECT data_inicio FROM oferta WHERE morada = NEW.morada AND codigo = NEW.codigo)
-		THEN
-			INSERT INTO oferta (morada, codigo, data_inicio, data_fim, tarifa)
-			VALUES (NEW.morada, NEW.codigo, NEW.data_inicio, NEW.data_fim, NEW.tarifa);
-		ELSE
-			SET error_message = 'Ja existe uma oferta associada a este edificio com esta data';
-			CALL error_message;
-		ENDIF;
-	END //
-DELIMITER ;
-
+		IF NEW.data_inicio in (select data_inicio from oferta where morada = NEW.morada and codigo = NEW.codigo)
+        THEN
+              set error_message = 'Ja existe uma oferta associada a este edificio com esta data';
+              CALL error_message;
+         ELSE
+              INSERT INTO oferta (morada, codigo, data_inicio, data_fim, tarifa) 
+	      values (NEW.morada, NEW.codigo, NEW.data_inicio, NEW.data_fim, NEW.tarifa);
+        END IF;
+END //
 
 --b)
-DELIMITER //
 CREATE TRIGGER ins_dataPagamento BEFORE INSERT ON paga
 	FOR EACH ROW
 	BEGIN
@@ -34,14 +32,13 @@ CREATE TRIGGER ins_dataPagamento BEFORE INSERT ON paga
 			INSERT INTO paga (numero, data, metodo) VALUES (NEW.numero, NEW.data, NEW.metodo);
 		ENDIF;
 
-	END //
-DELIMITER ;
-
--- STORED PROCEDURE / INSERT BUILDINGS -- DONE BY RAQUEL
-DELIMITER //
-CREATE PROCEDURE insertBuilding(IN @morada varchar(255))
-BEGIN
-   	INSERT INTO Edificio (morada) VALUES (@morada);
 END //
+DELIMITER ;	
+-- STORED PROCEDURE / INSERT BUILDINGS -- DONE BY RAQUEL
+--DELIMITER //
+--CREATE PROCEDURE insertBuilding(IN @morada varchar(255))
+--BEGIN
+   	--INSERT INTO Edificio (morada) VALUES (@morada);
+--END //
  
-DELIMITER ;
+--DELIMITER ;
