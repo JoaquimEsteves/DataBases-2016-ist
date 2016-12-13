8f676102a95bd22adb7ae6de451903d56619c2bc
RubenCorreia <ruben@casa-portuguesa.de>
2016-12-13 14:45:40 +0000

    Create cube.sql

---
+++ Delivery_4/cube.sql
@@ -0,0 +1,32 @@
+CREATE TABLE cube (
+    l integer,
+    d integer,
+    paga integer,
+    primary key(l,d),
+    forgein key (l) references local_dimension(local_id) UPDATE ON CASCADE,
+    forgein key (d) references data_dimension(data_id) UPDATE ON CASCADE);
+
+DELIMITER $$
+CREATE PROCEDURE insert_cube()
+    BEGIN
+        SET @l_id = 0;
+        SET @d_id = 0;
+
+        WHILE @l_id <= count(local_id)
+        DO
+          INSERT INTO cube (l,paga) VALUES (@l_id,(SELECT paga FROM local_dimension WHERE local_id = @l_id));
+          SET @l_id = @l_id+1;
+        END WHILE;
+
+        WHILE @d_id <= count(data_id)
+        DO
+          INSERT INTO cube (d) VALUES (@d_id);
+          SET @d_id = @d_id+1;
+        END WHILE;
+
+    END; $$
+DELIMITER ;
+
+call insert_cube;
+
+SELECT l,d, avg(paga) as valor_medio FROM cube GROUP BY CUBE (f,d);
