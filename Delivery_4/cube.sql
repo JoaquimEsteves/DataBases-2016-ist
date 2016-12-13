888d04f34d0930605f4a5d3741e9b81b8c2eed72
RubenCorreia <ruben@casa-portuguesa.de>
2016-12-13 15:18:14 +0000

    Update cube.sql

--- Delivery_4/cube.sql
+++ Delivery_4/cube.sql
@@ -18,14 +18,14 @@ CREATE PROCEDURE insert_cube()
         SET @count_local = (SELECT count(local_id) FROM local_dimension);
         
         IF  @count_local < @count_data THEN
-            WHILE @l_id < @count_local
+            WHILE @l_id <= @count_local
             DO
               INSERT INTO cube (l,d,paga) VALUES (@l_id,@d_id,(SELECT paga FROM local_dimension WHERE local_id = @l_id));
               SET @l_id = @l_id+1;
               SET @d_id = @d_id+1;
             END WHILE;
 
-            WHILE @d_id < @count_data 
+            WHILE @d_id <= @count_data 
             DO  
               INSERT INTO cube (d) VALUES (@d_id);
               SET @d_id = @d_id+1;
