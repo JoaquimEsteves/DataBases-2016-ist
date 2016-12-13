a30dca9ae5adeb740d0682c0364c0b6dcbc3cf78
RubenCorreia <ruben@casa-portuguesa.de>
2016-12-13 15:17:04 +0000

    Update cube.sql

--- Delivery_4/cube.sql
+++ Delivery_4/cube.sql
@@ -6,8 +6,8 @@ CREATE TABLE cube (
     d integer DEFAULT NULL,
     paga integer DEFAULT NULL,
     primary key(l,d),
-    foreign key  (l) references local_dimension(local_id),
-    foreign key (d) references data_dimension(data_id));
+    foreign key  (l) references local_dimension(local_id) ON UPDATE CASCADE,
+    foreign key (d) references data_dimension(data_id) ON UPDATE CASCADE);
 
 DELIMITER $$
 CREATE PROCEDURE insert_cube()
