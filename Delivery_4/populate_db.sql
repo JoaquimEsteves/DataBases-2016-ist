616ab49361504deaa225a828f4a326b1cd53b2fc
raquelpinto <raquelpintopereira6@gmail.com>
2016-12-14 12:53:41 +0000

    Update populate_db.sql

--- Delivery_4/populate_db.sql
+++ Delivery_4/populate_db.sql
@@ -18,7 +18,7 @@ BEGIN
     WHILE @i < 10000
     DO
     
-        SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 10) INTO ce;
+        SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 10) INTO codigo_espaco;
         SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 10) INTO morada;
         SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 10) INTO codigo;
         SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 10) INTO nif;
@@ -27,19 +27,21 @@ BEGIN
         SELECT CURDATE() - INTERVAL FLOOR(RAND() * 14) DAY INTO data_inicio;
         
         INSERT INTO edificio(morada) VALUES(morada);
-        INSERT INTO user(nif) VALUES(nif);
-        INSERT INTO fiscal(id) VALUES(@id);
         INSERT INTO alugavel(morada, codigo) VALUES(morada, codigo);
+        INSERT INTO oferta(morada, codigo, data_inicio) VALUES(morada, codigo, data_inicio);
+        
+        INSERT INTO user(nif) VALUES(nif);
+        INSERT INTO reserva(numero) VALUES(numero);
+        INSERT INTO aluga(morada, codigo, data_inicio, nif, numero) VALUES(morada, codigo,data_inicio, nif,numero);
+        
+        INSERT INTO fiscal(id) VALUES(@id);
         INSERT INTO arrenda(morada, codigo,nif) VALUES(morada, codigo,nif);
         INSERT INTO fiscaliza(id, morada, codigo) VALUES(id, morada, codigo);
         
-        INSERT INTO oferta(morada, codigo, data_inicio) VALUES(morada, codigo, data_inicio);
-        
-        INSERT INTO reserva(numero) VALUES(numero);
-        INSERT INTO estado(numero, time_stamp) VALUES(numero, time_stamp);
-        INSERT INTO aluga(morada, codigo, data_inicio, nif, numero) VALUES(morada, codigo,data_inicio, nif,numero);
         INSERT INTO espaco(morada, codigo) VALUES(morada, codigo);
         INSERT INTO posto(morada, codigo, codigo_espaco) VALUES(morada, codigo_espaco, codigo);
+        
+        INSERT INTO estado(numero, time_stamp) VALUES(numero, time_stamp);
         
         SET @i = @i+1;
         SET @id = @id+1;
