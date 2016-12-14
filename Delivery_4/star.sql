ba7752db2cbc2ceab4896452f68cf9a67377fca8
RubenCorreia <ruben@casa-portuguesa.de>
2016-12-14 14:47:29 +0000

    Update star.sql

--- Delivery_4/star.sql
+++ Delivery_4/star.sql
@@ -43,7 +43,7 @@ CREATE TABLE reservation_fact (
         user_nif varchar (9) NOT NULL,
         local_id integer NOT NULL,
         time_id integer NOT NULL,
-        date_id integer NOT NULL,
+        date_id timestamp NOT NULL,
         status_id integer NOT NULL,
         duration_in_days integer NOT NULL,
         primary key (reservation_id, user_nif, local_id, time_id, date_id, status_id),
