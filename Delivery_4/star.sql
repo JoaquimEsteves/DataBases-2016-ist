351b481f63c249864fe193444c557c2eb04dedc6
RubenCorreia <ruben@casa-portuguesa.de>
2016-12-14 14:26:10 +0000

    Update star.sql

--- Delivery_4/star.sql
+++ Delivery_4/star.sql
@@ -6,44 +6,44 @@ DROP TABLE IF EXISTS date_dimension;
 
 
 CREATE TABLE user_dimension (
-        nif varchar(9) NOT NULL UNIQUE,
-        nome varchar(80) NOT NULL,
-        telefone varchar(26) NOT NULL,
-        primary key(nif));
+        user_nif varchar (9) NOT NULL UNIQUE,
+        user_name varchar (80) NOT NULL,
+        user_telephone varchar (26) NOT NULL,
+        primary key(user_nif));
 
 CREATE TABLE local_dimension (
         local_id integer NOT NULL UNIQUE,
-        edificio varchar(255) NOT NULL ,
-        posto varchar(255) DEFAULT 'NO POSTO AVAILABLE',
-        espaco varchar(255) DEFAULT 'NO ESPACO AVAILABLE',
+        local_building varchar (255) NOT NULL,
+        local_post varchar (255) DEFAULT 'NO POSTO AVAILABLE',
+        local_space varchar (255) DEFAULT 'NO ESPACO AVAILABLE',
+        payed_amount numeric (19,4) NOT NULL,
         primary key (local_id));
 
 CREATE TABLE time_dimension (
-        tempo_id integer NOT NULL UNIQUE,
-        hora integer,
-        minuto integer,
-        primary key (tempo_id));
+        time_id integer NOT NULL UNIQUE,
+        time_hour integer NOT NULL,
+        time_minute integer NOT NULL,
+        primary key (time_id));
 
 CREATE TABLE date_dimension (
-        data_id integer NOT NULL UNIQUE,
-        dia integer,
-        semana integer,
-        mes varchar(255),
-        ano integer,
-        semestre integer,
-        primary key(data_id));
+        date_id integer NOT NULL UNIQUE,
+        date_day integer NOT NULL,
+        date_week integer NOT NULL,
+        date_month varchar (255) NOT NULL,
+        date_year integer NOT NULL,
+        date_semester integer NOT NULL,
+        primary key(date_id));
 
 CREATE TABLE reservation_fact (
-        numero varchar(255) NOT NULL UNIQUE,
-        nif varchar(9) NOT NULL,
+        reservation_id varchar (255) NOT NULL UNIQUE,
+        user_nif varchar (9) NOT NULL,
         local_id integer NOT NULL,
-        tempo_id integer NOT NULL,
-        data_id integer NOT NULL,
-        estado varchar(255) NOT NULL,
-        paga numeric (19,4) NOT NULL,
-        duracao integer NOT NULL,
-        primary key(numero, nif, local_id, tempo_id, data_id),
-        foreign key (nif) references user_dimension(nif) ON DELETE CASCADE,
+        time_id integer NOT NULL,
+        date_id integer NOT NULL,
+        status varchar (255) NOT NULL,
+        duration_in_days integer NOT NULL,
+        primary key (reservation_id, user_nif, local_id, time_id, date_id),
+        foreign key (user_nif) references user_dimension(user_nif) ON DELETE CASCADE,
         foreign key (local_id) references local_dimension(local_id) ON DELETE CASCADE,
-        foreign key (tempo_id) references time_dimension(tempo_id) ON DELETE CASCADE,
-        foreign key (data_id) references date_dimension(data_id) ON DELETE CASCADE);
+        foreign key (time_id) references time_dimension(time_id) ON DELETE CASCADE,
+        foreign key (date_id) references date_dimension(date_id) ON DELETE CASCADE);
